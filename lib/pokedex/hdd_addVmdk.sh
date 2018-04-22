f49e542c180d07d47435ebb42c3c7c6ae7a86ce7
BwayCer <bwaycer@gmail.com>
2018-04-22 15:46:01 +0800

    優化 把 pokedex.sh、hdd_addVmdk.sh 套上樣本腳本

--- lib/pokedex/hdd_addVmdk.sh
+++ lib/pokedex/hdd_addVmdk.sh
@@ -6,76 +6,131 @@
 # https://github.com/BwayCer/ditto.vm/blob/Rotom/Rotom/pokedex/README.md#問題集-1
 
 
-# ./hdd_addVmdk.sh \
-#     [--noconfirm] \
-#     <目標目錄>
-#     [<磁區名稱 [A-Za-z0-9_]>]
-#     <單磁區大小 (0008|0128|0512|4064)>
-#     [<起始編號 (1-999)>]
-#     <數量 (1-999)>
+##shStyle ###
 
 
-__filename=`realpath "$0"`
-_dirsh=`dirname "$__filename"`
-_binsh=`realpath "$_dirsh/../../bin"`
-_libsh=`realpath "$_dirsh/.."`
-_fileName=`basename "$0"`
+_originPlace="lib"
 
-
-opt_simpleCheck=0
-opt_noConfirm=0
-fnMain() {
-    local opt val
-
-    while [ -n 1 ]
-    do
-        opt="$1"
-        val="$2"
-
-        if [ -z "`echo "$opt" | grep "^-"`" ]; then
-            for opt in "$@"
-            do
-                if [ -n "`echo "$opt" | grep "^-"`" ]; then
-                    echo "[錯誤] 命令用法： \`<命令> [選項] [參數]\`。"
-                    exit 1
-                fi
-            done
-
-            break
-        fi
-
-        case "$opt" in
-            "--simpleCheck" )
-                opt_simpleCheck=1
-                shift
-                ;;
-            "--noconfirm" )
-                opt_noConfirm=1
-                shift
-                ;;
-            * )
-                echo "[錯誤] 非法選項 $opt"
-                exit 1
-                ;;
-        esac
-    done
-
-    fnMainHandle "$@"
+_shScript() {
+    case "$1" in
+    esac
 }
 
-fnMainHandle() {
-    local vhddDirPath=`"$_libsh/basesh/path.resolve" "$1"`
-    local name="$2"
-    local grainSizeM="$3"
-    local startNum=$4
-    local totalNum=$5
 
-    if [ $opt_simpleCheck -eq 1 ]; then
-        shift
-        echo $# "$vhddDirPath"
-        exit
+##shStyle 介面函式
+
+
+fnHelp_main() { echo "# 新增虛擬硬碟。
+# 磁區名稱： [A-Za-z0-9_]
+# 單磁區大小： (0008|0128|0512|4064)
+# 起始編號： (1-999)
+# 數量： (1-999)
+[[USAGE]] <目標目錄>
+      < [<磁區名稱>] <單磁區大小> [<起始編號>] <數量>
+      | <[<磁區名稱>:]<單磁區大小>:[<起始編號>:]<數量>[ ...]> >
+[[OPT]]
+      --noconfirm   忽略確認對話。
+  -h, --help        幫助。
+"; }
+fnOpt_main() {
+    case "$1" in
+        --noconfirm )
+            opt_noconfirm=1
+            return 1
+            ;;
+        -h | --help ) fnShowHelp ;;
+        * ) return 3 ;;
+    esac
+}
+fnMain() {
+    opt_noconfirm=0
+    fnParseOption
+
+    local vhddDirPath=`"$_libsh/basesh/path.resolve" "${_args[0]}"`
+    local otherParameter=("${_args[@]:1}")
+
+    local val
+    local regexParseOptionA="^\(\([A-Za-z][A-Za-z0-9_]*\) \+\)\?"
+    regexParseOptionA+="\(0008\|0128\|0512\|4064\) \+"
+    regexParseOptionA+="\(\([1-9][0-9]\{,2\}\) \+\)\?"
+    regexParseOptionA+="\([1-9][0-9]\{,2\}\) *$"
+    local regexParseOptionB="^\(\([A-Za-z][A-Za-z0-9_]*\):\)\?"
+    regexParseOptionB+="\(0008\|0128\|0512\|4064\):"
+    regexParseOptionB+="\(\([1-9][0-9]\{,2\}\):\)\?"
+    regexParseOptionB+="\([1-9][0-9]\{,2\}\)$"
+
+    if   [ -n "`echo "${otherParameter[@]}" | grep "$regexParseOptionA"`" ]; then
+        printf "";
+    elif [ -n "`echo "${otherParameter[0]}" | grep "$regexParseOptionB"`" ]; then
+        for val in "${otherParameter[@]}"
+        do
+            if [ -z "`echo "$val" | grep "$regexParseOptionB"`" ]; then
+                Loxog err "[錯誤] 請再次檢查參數。"
+                exit 1
+            fi
+        done
+    else
+        Loxog err "[錯誤] 請再次檢查參數。"
+        exit 1
     fi
 
+    if [ ! -f "$vhddDirPath/info.txt" ]; then
+        mkdir -p "$vhddDirPath" "$vhddDirPath/vHDD/"
+        if [ $? -eq 1 ]; then
+            Loxog err "[錯誤] 無法在指定路徑建立目標目錄。"
+            exit 1
+        fi
+
+        cat /dev/null > "$vhddDirPath/info.txt"
+        touch "$vhddDirPath/interface.vmdk"
+    elif [ ! -d "$vhddDirPath/vHDD/" ]; then
+        mkdir "$vhddDirPath/vHDD/"
+    fi
+
+    "$_dirsh/hdd_info_check.sh" "$vhddDirPath"
+    if [ $? -eq 1 ]; then exit 1; fi
+
+
+    if [ -n "`echo "${otherParameter[@]}" | grep "$regexParseOptionA"`" ]; then
+        addVmdk $opt_noconfirm "$vhddDirPath" \
+            "`echo "$@" | sed "s/$regexParseOptionA/\2/"`" \
+            "`echo "$@" | sed "s/$regexParseOptionA/\3/"`" \
+            "`echo "$@" | sed "s/$regexParseOptionA/\5/"`" \
+            "`echo "$@" | sed "s/$regexParseOptionA/\6/"`"
+        if [ $? -eq 1 ]; then exit 1; fi
+    elif [ -n "`echo "${otherParameter[0]}" | grep "$regexParseOptionB"`" ]; then
+        for val in "${otherParameter[@]}"
+        do
+            addVmdk $opt_noconfirm "$vhddDirPath" \
+                "`echo "$val" | sed "s/$regexParseOptionB/\2/"`" \
+                "`echo "$val" | sed "s/$regexParseOptionB/\3/"`" \
+                "`echo "$val" | sed "s/$regexParseOptionB/\5/"`" \
+                "`echo "$val" | sed "s/$regexParseOptionB/\6/"`"
+            if [ $? -eq 1 ]; then exit 1; fi
+        done
+    fi
+
+
+    echo -e "\n\ninfo.txt"
+    "$_dirsh/pokedex.sh" hdd info "$vhddDirPath"
+}
+
+
+##shStyle 共享變數
+
+
+
+##shStyle 函式庫
+
+
+addVmdk() {
+    local tmp
+    local bisNoConfirm=$1
+    local vhddDirPath="$2"
+    local name="$3"
+    local grainSizeM="$4"
+    local startNum=$5
+    local totalNum=$6
 
     local regexParseOption
     regexParseOption="^\([A-Za-z][A-Za-z0-9_]*\)\? \(0008\|0128\|0512\|4064\) "
@@ -93,14 +148,15 @@ fnMainHandle() {
         exit 1
     fi
 
-    if [ -z "`echo "$name $grainSizeM $startNum $totalNum" | grep "$regexParseOption"`" ]; then
+
+    tmp=`echo "$name $grainSizeM $startNum $totalNum" | grep "$regexParseOption"`
+    if [ -z "$tmp" ]; then
         tmpErrMsg="[錯誤] 請再次檢查參數。"
         echo -e "\e[01;31m${tmpErrMsg}\e[00m"
         exit 1
     fi
 
 
-    local tmp
     local amount totalSize totalGrainSizeM
     local vhddInfo vhddInfoSummary txtVhddInfo
     local bisHasOriginVhddInfo=0
@@ -117,7 +173,8 @@ fnMainHandle() {
     totalGrainSizeM=`echo "$vhddInfoSummary" | cut -d " " -f 6`
     bisHasOriginVhddInfo=$totalGrainSizeM
 
-    tmp="amount: \(0\|[1-9][0-9]*\) totalSize: \(0\|[1-9][0-9]*\) totalGrainSizeM: \(0\|[1-9][0-9]*\)"
+    tmp="amount: \(0\|[1-9][0-9]*\)"
+    tmp+=" totalSize: \(0\|[1-9][0-9]*\) totalGrainSizeM: \(0\|[1-9][0-9]*\)"
     if [ -z "`echo "$vhddInfoSummary" | grep "^$tmp$"`" ]; then
         tmpErrMsg="[錯誤] \".pokedex_hddInfo.tmp\" 文件的概括資訊與期望不符。"
         echo -e "\e[01;31m${tmpErrMsg}\e[00m"
@@ -147,11 +204,13 @@ fnMainHandle() {
     fi
 
 
-    if [ $opt_noConfirm -eq 0 ] && [ $bisHasOriginVhddInfo -ne 0 ]; then
+    if [ $bisNoConfirm -eq 0 ] && [ $bisHasOriginVhddInfo -ne 0 ]; then
         fnCheckExistFile "$vhddDirPath" $startNum $endNum "$txtVhddInfo"
         amount=$(( $amount - $fnCheckExistFile_subtractionAmount ))
         totalSize=$(( $totalSize - $fnCheckExistFile_subtractionTotalSize ))
-        totalGrainSizeM=$(( $totalGrainSizeM - $fnCheckExistFile_subtractionTotalGrainSizeM ))
+        totalGrainSizeM=$((
+            $totalGrainSizeM - $fnCheckExistFile_subtractionTotalGrainSizeM
+        ))
         txtVhddInfo="$fnCheckExistFile_vhddInfo"
     fi
 
@@ -181,7 +240,6 @@ fnMainHandle() {
     fnGetVmdkInterface $totalGrainSizeM "`echo -e "$txtVhddInfo" | cut -d " " -f 9-`"
     echo -e "$rtnGetVmdkInterface" > "$vhddDirPath/interface.vmdk"
 }
-
 
 fnRemindUpdateInfo() {
     local vhddDirPath="$1"
@@ -388,5 +446,265 @@ ddb.virtualHWVersion = "12"'
 }
 
 
-fnMain "$@"
+##shStyle 腳本環境
+
+
+__filename=`realpath "$0"`
+_dirsh=`dirname "$__filename"`
+[ "$_originPlace" == "bin" ] && _binsh=$_dirsh || _binsh=`realpath "$_dirsh/../../bin"`
+[ "$_originPlace" == "bin" ] && _libsh=`realpath "$_dirsh/../lib"` || _libsh=`realpath "$_dirsh/.."`
+_fileName=`basename "$0"`
+
+_PWD=$PWD
+_br="
+"
+
+# 0 黑 black
+# 1 紅 red
+# 2 綠 green
+# 3 黃 yellow
+# 4 藍 blue
+# 5 粉 magenta
+# 6 青 cyan
+# 7 白 white
+_fColor() {
+    local color=$1
+    local bold=$2
+    local bgcolor=$3
+    local underline=$4
+
+    [ $_fColor_usable -eq 0 ] && [ $_fColor_force -eq 0 ] && return
+
+    if [ "$color" == "N" ]; then
+        [ $_fColor_force -eq 0 ] \
+            && printf "`tput sgr0`" \
+            || printf "\e[00m"
+
+        return
+    fi
+
+    case "$color" in
+        [01234567] )
+            [ $_fColor_force -eq 0 ] \
+                && printf "`tput setaf $color`" \
+                || printf "\e[3${color}m"
+            ;;
+    esac
+
+    case "$bgcolor" in
+        [01234567] )
+            [ $_fColor_force -eq 0 ] \
+                && printf "`tput setab $bgcolor`" \
+                || printf "\e[4${bgcolor}m"
+            ;;
+    esac
+
+    if [ "$bold" == "1" ]; then
+        [ $_fColor_force -eq 0 ] \
+            && printf "`tput bold`" \
+            || printf "\e[01m"
+    fi
+
+    if [ "$underline" == "1" ]; then
+        [ $_fColor_force -eq 0 ] \
+            && printf "`tput smul`" \
+            || printf "\e[04m"
+    fi
+}
+_fColor_usable=0
+_fColor_force=0
+
+_fN=""
+_fRedB=""
+_fYelB=""
+
+Loxog() {
+    local _stdin=`[ ! -t 0 ] && { \
+        IFS='';
+        while read pipeData; do echo "$pipeData"; done <&0;
+        unset IFS
+    }`
+    local method="$1"; shift
+
+    local color formatArgus
+
+    case $method in
+        com ) color=$_fN    ;; # common
+        war ) color=$_fYelB ;; # warn
+        err ) color=$_fRedB ;; # error
+    esac
+
+    formatArgus="$color%s$_fN\n"
+
+    local idx val len
+
+    [ -n "$*" ] && printf "$formatArgus" "$@" 1>&2
+
+    [ -z "$_stdin" ] && return
+    len=`echo "$_stdin" | wc -l`
+    for idx in `seq 1 $len`
+    do
+        val=`echo "$_stdin" | sed -n "${idx}p"`
+        printf "$formatArgus" "$val" 1>&2
+    done
+}
+
+_onCtrlC() {
+    local val
+    for val in "$@"
+    do
+        _onCtrlC_cmd+=$val$_br
+    done
+    trap 'sh -c "echo ; $_onCtrlC_cmd echo"; exit' 2
+}
+_onCtrlC_cmd=""
+
+
+##shStyle ###
+
+
+[ -L "$0" ] && exec "`realpath "$0"`" "$@"
+
+_stdin=`[ ! -t 0 ] && while read pipeData; do echo $pipeData; done <&0`
+
+_args=("$@")
+_origArgs=("$@")
+
+argsShift() {
+    local amount=$1
+
+    if [ -z "$amount" ] || [ $amount -lt 1 ]; then amount=1; fi
+    _args=("${_args[@]:$amount}")
+}
+
+tmp=`tput colors`
+[ -t 1 ] && [ -n "$tmp" ] && [ $tmp -ge 8 ] && _fColor_usable=1
+_fnForceColor() {
+    [ "$1" == 1 ] && _fColor_force=1
+    _fN=`_fColor N`
+    _fRedB=`_fColor 1 1`
+    _fYelB=`_fColor 3 1`
+}
+_fnForceColor
+
+fnParseOption() {
+    local fnHandleOpt="fnOpt_$_shCmd"
+
+    local tmp args opt val cutLen errMsg
+    args=("${_args[@]}")
+    errMsg=""
+
+    while [ 1 ]
+    do
+        opt=${args[0]}
+        val=${args[1]}
+        cutLen=2
+
+        if [ "$opt" == "--" ] || [ -z "`echo "_$opt" | grep "^_-"`" ]; then break; fi
+
+        if [ -n "`echo "_$opt" | grep "^_-[^-]"`" ] && [ ${#opt} -ne 2 ]; then
+            tmp="-"${opt:2}
+            opt=${opt:0:2}
+            val=""
+            cutLen=1
+            args=("$opt" "$tmp" "${args[@]:1}")
+        elif [ -n "`echo "_$val" | grep "^_-"`" ]; then
+            val=""
+            cutLen=1
+        fi
+
+        if [ "$opt" == "--color" ]; then
+            _fnForceColor 1
+            tmp=1
+        else
+            $fnHandleOpt "$opt" "$val"
+            tmp=$?
+        fi
+        case $tmp in
+            0 )
+                echo '請檢查 "'$fnHandleOpt'" 的錯誤回傳值。' 1>&2
+                exit
+                ;;
+            # 使用 1 個參數
+            1 )
+                [ $cutLen -eq 2 ] && (( cutLen-- ))
+                ;;
+            # 使用 2 個參數
+            2 ) ;;
+            3 )
+                errMsg+=$_br'找不到 "'$opt'" 選項。'
+                ;;
+            4 )
+                [ "$val" == "" ] && val="null" || val='"'$val'"'
+                errMsg+=$_br$val' 不符合 "'$opt'" 選項的預期值。'
+                ;;
+        esac
+
+        args=("${args[@]:$cutLen}")
+    done
+
+    if [ "${args[0]}" == "--" ]; then
+        args=("${args[@]:1}")
+    else
+        for val in "${args[@]}"
+        do
+            [ -z "`echo "_$val" | grep "^_-"`" ] && continue
+
+            errMsg+=$_br'不符合 "[命令] [選項] [參數]" 的命令用法。'
+            break
+        done
+    fi
+
+    if [ -z "$errMsg" ]; then
+        _args=("${args[@]}")
+        return
+    fi
+
+    echo "$errMsg" | sed "1d" \
+        | sed "s/^\(.\)/[$_fileName]: \1/" \
+        | Loxog err
+    exit 1
+}
+
+fnShowHelp() {
+    local txtHelp=`fnHelp_$_shCmd`
+
+    local bisUsage bisSubCmd bisOpt
+    local usage
+    bisUsage=` echo "$txtHelp" | grep "\[\[USAGE\]\]"`
+    bisSubCmd=`echo "$txtHelp" | grep "\[\[SUBCMD\]\]"`
+    bisOpt=`   echo "$txtHelp" | grep "\[\[OPT\]\]"`
+
+    if [ -n "$bisUsage" ]; then
+        usage="用法："
+        [ -n "$bisSubCmd" ] && usage+=" [命令]"
+        [ -n "$bisOpt" ] && usage+=" [選項]"
+
+        txtHelp=`echo "$txtHelp" | sed "s/\[\[USAGE\]\]/\n$usage/"`
+    fi
+
+    [ -n "$bisSubCmd" ] && \
+        txtHelp=`echo "$txtHelp" | sed "s/\[\[SUBCMD\]\]/\\n\\n命令：\\n/"`
+
+    [ -n "$bisOpt" ] && \
+        txtHelp=`echo "$txtHelp" | sed "s/\[\[OPT\]\]/\\n\\n選項：\\n/"`
+
+    echo "$txtHelp$_br"
+    exit
+}
+
+
+_shCmd=""
+_shCmdLevel=0
+_shScript "$@"
+if [ $_shCmdLevel -eq 0 ]; then
+    _shCmd="main"
+    fnMain "$@"
+    exit
+fi
+for tmp in `seq 0 $(( $_shCmdLevel -1 ))`
+do _shCmd+="_${_args[ $tmp ]}"; done
+argsShift $_shCmdLevel
+_shCmd=${_shCmd:1}
+fnMain_$_shCmd "${_args[@]}"
 
--- lib/pokedex/pokedex.sh
+++ lib/pokedex/pokedex.sh
@@ -2,9 +2,375 @@
 # 圖鑑
 
 
-if [ -L "$0" ]; then
-    originFileName=`realpath "$0"`
-    if [ ! -x "$originFileName" ]; then exit 1; fi
-    exec "$originFileName" "$@"
+##shStyle ###
+
+
+_originPlace="lib"
+
+_shScript() {
+    case "$1" in
+    dir ) ;;
+    hdd ) _shCmdLevel=1 ;
+        case "$2" in
+        info ) _shCmdLevel=2 ;;
+        add  ) _shCmdLevel=2 ;;
+        esac ;;
+    machine | mac | mach | machi | machin | machine ) ;;
+    host | hos ) ;;
+    diff ) ;;
+    esac
+}
+
+
+##shStyle 介面函式
+
+
+fnHelp_main() { echo "# 圖鑑
+# vmware 虛擬機操作工具。
+[[USAGE]] [多個參數]
+[[SUBCMD]]
+  dir        製作中...
+  hdd        $fnHelp_hdd_briefly
+  machine    製作中...
+  host       製作中...
+  diff       製作中...
+[[OPT]]
+  -h, --help   幫助。
+"; }
+fnOpt_main() {
+    case "$1" in
+        -h | --help ) fnShowHelp ;;
+        * ) return 3 ;;
+    esac
+}
+fnMain() {
+    fnParseOption
+
+    fnShowHelp
+}
+
+fnHelp_hdd_briefly="虛擬硬碟。"
+fnHelp_hdd() { echo "# $fnHelp_hdd_briefly
+[[USAGE]]
+[[SUBCMD]]
+  list    製作中...
+  info    $fnHelp_hdd_info_briefly
+  add     $fnHelp_hdd_add_briefly
+  rm      製作中...
+[[OPT]]
+  -h, --help   幫助。
+"; }
+fnOpt_hdd() {
+    case "$1" in
+        -h | --help ) fnShowHelp ;;
+        * ) return 3 ;;
+    esac
+}
+fnMain_hdd() {
+    fnParseOption
+
+    fnShowHelp
+}
+
+fnHelp_hdd_info_briefly="顯示硬碟資訊。"
+fnHelp_hdd_info() { echo "# $fnHelp_hdd_info_briefly
+[[USAGE]] <vhddPath>
+[[OPT]]
+      --update   更新。
+  -h, --help     幫助。
+"; }
+fnOpt_hdd_info() {
+    case "$1" in
+        --update )
+            opt_update="--update"
+            return 1
+            ;;
+        -h | --help ) fnShowHelp ;;
+        * ) return 3 ;;
+    esac
+}
+fnMain_hdd_info() {
+    opt_update=""
+    fnParseOption
+    "$_dirsh/hdd_info.sh" $opt_update "${_args[@]}"
+}
+
+fnHelp_hdd_add_briefly="新增。"
+# fnHelp_hdd_add() {}
+# fnOpt_hdd_add() {}
+fnMain_hdd_add() {
+    # fnParseOption
+    "$_dirsh/hdd_addVmdk.sh" "$@"
+}
+
+
+##shStyle 共享變數
+
+
+
+##shStyle 函式庫
+
+
+
+##shStyle 腳本環境
+
+
+__filename=`realpath "$0"`
+_dirsh=`dirname "$__filename"`
+[ "$_originPlace" == "bin" ] && _binsh=$_dirsh || _binsh=`realpath "$_dirsh/../../bin"`
+[ "$_originPlace" == "bin" ] && _libsh=`realpath "$_dirsh/../lib"` || _libsh=`realpath "$_dirsh/.."`
+_fileName=`basename "$0"`
+
+_PWD=$PWD
+_br="
+"
+
+# 0 黑 black
+# 1 紅 red
+# 2 綠 green
+# 3 黃 yellow
+# 4 藍 blue
+# 5 粉 magenta
+# 6 青 cyan
+# 7 白 white
+_fColor() {
+    local color=$1
+    local bold=$2
+    local bgcolor=$3
+    local underline=$4
+
+    [ $_fColor_usable -eq 0 ] && [ $_fColor_force -eq 0 ] && return
+
+    if [ "$color" == "N" ]; then
+        [ $_fColor_force -eq 0 ] \
+            && printf "`tput sgr0`" \
+            || printf "\e[00m"
+
+        return
+    fi
+
+    case "$color" in
+        [01234567] )
+            [ $_fColor_force -eq 0 ] \
+                && printf "`tput setaf $color`" \
+                || printf "\e[3${color}m"
+            ;;
+    esac
+
+    case "$bgcolor" in
+        [01234567] )
+            [ $_fColor_force -eq 0 ] \
+                && printf "`tput setab $bgcolor`" \
+                || printf "\e[4${bgcolor}m"
+            ;;
+    esac
+
+    if [ "$bold" == "1" ]; then
+        [ $_fColor_force -eq 0 ] \
+            && printf "`tput bold`" \
+            || printf "\e[01m"
+    fi
+
+    if [ "$underline" == "1" ]; then
+        [ $_fColor_force -eq 0 ] \
+            && printf "`tput smul`" \
+            || printf "\e[04m"
+    fi
+}
+_fColor_usable=0
+_fColor_force=0
+
+_fN=""
+_fRedB=""
+_fYelB=""
+
+Loxog() {
+    local _stdin=`[ ! -t 0 ] && { \
+        IFS='';
+        while read pipeData; do echo "$pipeData"; done <&0;
+        unset IFS
+    }`
+    local method="$1"; shift
+
+    local color formatArgus
+
+    case $method in
+        com ) color=$_fN    ;; # common
+        war ) color=$_fYelB ;; # warn
+        err ) color=$_fRedB ;; # error
+    esac
+
+    formatArgus="$color%s$_fN\n"
+
+    local idx val len
+
+    [ -n "$*" ] && printf "$formatArgus" "$@" 1>&2
+
+    [ -z "$_stdin" ] && return
+    len=`echo "$_stdin" | wc -l`
+    for idx in `seq 1 $len`
+    do
+        val=`echo "$_stdin" | sed -n "${idx}p"`
+        printf "$formatArgus" "$val" 1>&2
+    done
+}
+
+_onCtrlC() {
+    local val
+    for val in "$@"
+    do
+        _onCtrlC_cmd+=$val$_br
+    done
+    trap 'sh -c "echo ; $_onCtrlC_cmd echo"; exit' 2
+}
+_onCtrlC_cmd=""
+
+
+##shStyle ###
+
+
+[ -L "$0" ] && exec "`realpath "$0"`" "$@"
+
+_stdin=`[ ! -t 0 ] && while read pipeData; do echo $pipeData; done <&0`
+
+_args=("$@")
+_origArgs=("$@")
+
+argsShift() {
+    local amount=$1
+
+    if [ -z "$amount" ] || [ $amount -lt 1 ]; then amount=1; fi
+    _args=("${_args[@]:$amount}")
+}
+
+tmp=`tput colors`
+[ -t 1 ] && [ -n "$tmp" ] && [ $tmp -ge 8 ] && _fColor_usable=1
+_fnForceColor() {
+    [ "$1" == 1 ] && _fColor_force=1
+    _fN=`_fColor N`
+    _fRedB=`_fColor 1 1`
+    _fYelB=`_fColor 3 1`
+}
+_fnForceColor
+
+fnParseOption() {
+    local fnHandleOpt="fnOpt_$_shCmd"
+
+    local tmp args opt val cutLen errMsg
+    args=("${_args[@]}")
+    errMsg=""
+
+    while [ 1 ]
+    do
+        opt=${args[0]}
+        val=${args[1]}
+        cutLen=2
+
+        if [ "$opt" == "--" ] || [ -z "`echo "_$opt" | grep "^_-"`" ]; then break; fi
+
+        if [ -n "`echo "_$opt" | grep "^_-[^-]"`" ] && [ ${#opt} -ne 2 ]; then
+            tmp="-"${opt:2}
+            opt=${opt:0:2}
+            val=""
+            cutLen=1
+            args=("$opt" "$tmp" "${args[@]:1}")
+        elif [ -n "`echo "_$val" | grep "^_-"`" ]; then
+            val=""
+            cutLen=1
+        fi
+
+        if [ "$opt" == "--color" ]; then
+            _fnForceColor 1
+            tmp=1
+        else
+            $fnHandleOpt "$opt" "$val"
+            tmp=$?
+        fi
+        case $tmp in
+            0 )
+                echo '請檢查 "'$fnHandleOpt'" 的錯誤回傳值。' 1>&2
+                exit
+                ;;
+            # 使用 1 個參數
+            1 )
+                [ $cutLen -eq 2 ] && (( cutLen-- ))
+                ;;
+            # 使用 2 個參數
+            2 ) ;;
+            3 )
+                errMsg+=$_br'找不到 "'$opt'" 選項。'
+                ;;
+            4 )
+                [ "$val" == "" ] && val="null" || val='"'$val'"'
+                errMsg+=$_br$val' 不符合 "'$opt'" 選項的預期值。'
+                ;;
+        esac
+
+        args=("${args[@]:$cutLen}")
+    done
+
+    if [ "${args[0]}" == "--" ]; then
+        args=("${args[@]:1}")
+    else
+        for val in "${args[@]}"
+        do
+            [ -z "`echo "_$val" | grep "^_-"`" ] && continue
+
+            errMsg+=$_br'不符合 "[命令] [選項] [參數]" 的命令用法。'
+            break
+        done
+    fi
+
+    if [ -z "$errMsg" ]; then
+        _args=("${args[@]}")
+        return
+    fi
+
+    echo "$errMsg" | sed "1d" \
+        | sed "s/^\(.\)/[$_fileName]: \1/" \
+        | Loxog err
+    exit 1
+}
+
+fnShowHelp() {
+    local txtHelp=`fnHelp_$_shCmd`
+
+    local bisUsage bisSubCmd bisOpt
+    local usage
+    bisUsage=` echo "$txtHelp" | grep "\[\[USAGE\]\]"`
+    bisSubCmd=`echo "$txtHelp" | grep "\[\[SUBCMD\]\]"`
+    bisOpt=`   echo "$txtHelp" | grep "\[\[OPT\]\]"`
+
+    if [ -n "$bisUsage" ]; then
+        usage="用法："
+        [ -n "$bisSubCmd" ] && usage+=" [命令]"
+        [ -n "$bisOpt" ] && usage+=" [選項]"
+
+        txtHelp=`echo "$txtHelp" | sed "s/\[\[USAGE\]\]/\n$usage/"`
+    fi
+
+    [ -n "$bisSubCmd" ] && \
+        txtHelp=`echo "$txtHelp" | sed "s/\[\[SUBCMD\]\]/\\n\\n命令：\\n/"`
+
+    [ -n "$bisOpt" ] && \
+        txtHelp=`echo "$txtHelp" | sed "s/\[\[OPT\]\]/\\n\\n選項：\\n/"`
+
+    echo "$txtHelp$_br"
+    exit
+}
+
+
+_shCmd=""
+_shCmdLevel=0
+_shScript "$@"
+if [ $_shCmdLevel -eq 0 ]; then
+    _shCmd="main"
+    fnMain "$@"
+    exit
 fi
+for tmp in `seq 0 $(( $_shCmdLevel -1 ))`
+do _shCmd+="_${_args[ $tmp ]}"; done
+argsShift $_shCmdLevel
+_shCmd=${_shCmd:1}
+fnMain_$_shCmd "${_args[@]}"
 
